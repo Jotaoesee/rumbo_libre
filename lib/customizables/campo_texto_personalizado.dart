b7704b06f96c0c143756ba94bff5d51d7cc91f5b
Jotaoesee <jadiazm1995@gmail.com>
2025-01-14 21:38:05 +0100

    añado opcion para que la contraseña presionando el icono del ojo sea visible

--- lib/customizables/campo_texto_personalizado.dart
+++ lib/customizables/campo_texto_personalizado.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CampoTextoPersonalizado extends StatelessWidget {
+class CampoTextoPersonalizado extends StatefulWidget {
   final String etiqueta; // Etiqueta para el campo
   final IconData icono; // Ícono del campo
   final TextEditingController controlador; // Controlador para manejar el texto
@@ -17,15 +17,39 @@ class CampoTextoPersonalizado extends StatelessWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
+  _CampoTextoPersonalizadoState createState() =>
+      _CampoTextoPersonalizadoState();
+}
+
+class _CampoTextoPersonalizadoState extends State<CampoTextoPersonalizado> {
+  bool _mostrarContrasenia =
+      false; // Variable para controlar la visibilidad de la contraseña
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controlador,
-      obscureText: esContrasenia,
-      validator: validador,
+      controller: widget.controlador,
+      obscureText: widget.esContrasenia &&
+          !_mostrarContrasenia, // Controla si el campo es de contraseña
+      validator: widget.validador,
       decoration: InputDecoration(
-        labelText: etiqueta,
+        labelText: widget.etiqueta,
         labelStyle: const TextStyle(color: Colors.white70),
-        prefixIcon: Icon(icono, color: Colors.white),
+        prefixIcon: Icon(widget.icono, color: Colors.white),
+        suffixIcon: widget.esContrasenia
+            ? IconButton(
+                icon: Icon(
+                  _mostrarContrasenia ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _mostrarContrasenia = !_mostrarContrasenia;
+                  });
+                },
+              )
+            : null,
         filled: true,
         fillColor: Colors.white.withOpacity(0.2),
         border: OutlineInputBorder(
